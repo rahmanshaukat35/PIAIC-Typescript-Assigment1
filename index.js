c688474d06b812aaa396611206ed5a21a8c5a2eb
Al-fateh <rahmanshaukat35@gmail.com>
2023-07-22 22:45:50 +0500

    all examples done

--- index.js
+++ index.js
@@ -208,3 +208,340 @@ console.log('List of Famous Landmarks:');
 for (var i = 0; i < landmarking.length; i++) {
     console.log(landmarking[i]);
 }
+// exercise 23
+var car = 'subaru';
+console.log("Is car == 'subaru'? I predict true.");
+console.log(car == 'subaru');
+console.log("Is car == 'honda'? I predict false.");
+console.log(car == 'honda');
+console.log("Is car != 'toyota'? I predict true.");
+console.log(car != 'toyota');
+console.log("Is car != 'subaru'? I predict false.");
+console.log(car != 'subaru');
+var age = 18;
+console.log('Is age >= 18? I predict true.');
+console.log(age >= 18);
+console.log('Is age < 21? I predict true.');
+console.log(age < 21);
+var name1 = 'Alice';
+console.log("Does name start with letter 'A'? I predict True.");
+console.log(name1 == 'Alice'); // true
+var name2 = 'Bob';
+console.log("Does name end with letter 'a'? I predict False.");
+console.log(name2 == 'Alice'); // false
+// exercise 24
+// Tests for equality and inequality with strings:
+var string1 = 'Hello';
+var string2 = 'World';
+console.log(string1 === string2);
+console.log(string1 !== string2);
+// Tests using the lower case function:
+var uppercaseString = 'HELLO';
+var lowercaseString = 'hello';
+console.log(uppercaseString.toLowerCase() === lowercaseString);
+console.log(lowercaseString.toLowerCase() !== uppercaseString);
+// Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to:
+var num1 = 5;
+var num2 = 10;
+console.log(num1 === num2);
+console.log(num1 !== num2);
+console.log(num1 > num2);
+console.log(num1 < num2);
+console.log(num1 >= 5 && num2 <= 10);
+// Tests using "and" and "or" operators:
+var x = true;
+var y = false;
+console.log(x && y);
+console.log(x || y);
+// Test whether an item is in an array:
+var arrays = [1, 2, 3, 4, 5];
+var item1 = 3;
+var item2 = 6;
+var isItemInArray1 = false;
+for (var i = 0; i < arrays.length; i++) {
+    if (arrays[i] === item1) {
+        isItemInArray1 = true;
+        break;
+    }
+}
+console.log(isItemInArray1); // true
+// example 25
+var alien_color = 'green';
+// Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+if (alien_color === 'green') {
+    console.log('The player just earned 5 points.');
+}
+// Version that passes the if test
+alien_color = 'green'; // Change alien's color to green
+if (alien_color === 'green') {
+    console.log('The player just earned 15 points.'); // Output: The player just earned 5 points.
+}
+// Version that fails the if test
+alien_color = 'red'; // Change alien's color to red
+if (alien_color === 'green') {
+    console.log('The player just earned 10 points.'); // No output as this version fails the condition
+}
+// example 26
+var alien_colors = 'red';
+if (alien_colors === 'green') {
+    console.log('Congratulations! You just earned 5 points for shooting the alien.');
+}
+else {
+    console.log('Congratulations! You just earned 10 points for shooting the alien.');
+}
+// example 27
+var alien_color1 = 'yellow';
+if (alien_color1 === 'green') {
+    console.log('Congratulations! You just earned 5 points.');
+}
+else if (alien_color1 === 'yellow') {
+    console.log('Congratulations! You just earned 10 points.');
+}
+else if (alien_color1 === 'red') {
+    console.log('Congratulations! You just earned 15 points.');
+}
+// example 28
+var ages = 25;
+if (ages < 2) {
+    console.log('The person is a baby.');
+}
+else if (ages >= 2 && ages < 4) {
+    console.log('The person is a toddler.');
+}
+else if (ages >= 4 && ages < 13) {
+    console.log('The person is a kid.');
+}
+else if (ages >= 13 && ages < 20) {
+    console.log('The person is a teenager.');
+}
+else if (ages >= 20 && ages < 65) {
+    console.log('The person is an adult.');
+}
+else {
+    console.log('The person is an elder.');
+}
+// example 29
+var favoriteFruits = ['banana', 'apple', 'orange'];
+if (favoriteFruits[0] == 'banana') {
+    console.log('You really like bananas!');
+}
+if (favoriteFruits[1] == 'apple') {
+    console.log('You really like apples!');
+}
+if (favoriteFruits[2] == 'orange') {
+    console.log('You really like oranges!');
+}
+if (favoriteFruits[3] == 'strawberry') {
+    console.log('You really like strawberries!');
+}
+else {
+    console.log('Strawberries are not one of your favorite fruits.');
+}
+// example 30
+var usernames = ['admin', 'user1', 'user2', 'user3', 'user4'];
+for (var i = 0; i < usernames.length; i++) {
+    var username = usernames[i];
+    if (username === 'admin') {
+        console.log('Hello admin, would you like to see a status report?');
+    }
+    else {
+        console.log("Hello ".concat(username, ", thank you for logging in again."));
+    }
+}
+// example 31
+var users = [];
+if (users.length === 0) {
+    console.log('No Users: We need to find some users!');
+}
+else {
+    for (var i = 0; i < users.length; i++) {
+        var username = users[i];
+        console.log(username);
+    }
+}
+// example 32
+var current_users = ['John', 'Sarah', 'Mike', 'Emma', 'Alex'];
+var new_users = ['David', 'John', 'Emily', 'SARAH'];
+var _loop_1 = function (i) {
+    var usernameExists = current_users.some(function (user) { return user.toLowerCase() === new_users[i].toLowerCase(); });
+    if (usernameExists) {
+        console.log("Username '".concat(new_users[i], "' is not available. Please enter a new username."));
+    }
+    else {
+        console.log("Username '".concat(new_users[i], "' is available."));
+    }
+};
+for (var i = 0; i < new_users.length; i++) {
+    _loop_1(i);
+}
+// example 33
+var ordinalNumbers = [
+    '1st',
+    '2nd',
+    '3rd',
+    '4th',
+    '5th',
+    '6th',
+    '7th',
+    '8th',
+    '9th',
+];
+for (var i = 0; i < ordinalNumbers.length; i++) {
+    var result_1 = void 0;
+    if (i === 0) {
+        result_1 = "".concat(ordinalNumbers[i]);
+    }
+    else if (i === 1) {
+        result_1 = "".concat(ordinalNumbers[i]);
+    }
+    else if (i === 2) {
+        result_1 = "".concat(ordinalNumbers[i]);
+    }
+    else {
+        result_1 = "".concat(ordinalNumbers[i], "th");
+    }
+    console.log(result_1);
+}
+// example 34
+var pizzas = ['Pepperoni', 'Margherita', 'Hawaiian'];
+// Loop to print the name of each pizza
+for (var i = 0; i < pizzas.length; i++) {
+    console.log(pizzas[i]);
+}
+console.log('---');
+// Loop to print a sentence about each pizza
+for (var i = 0; i < pizzas.length; i++) {
+    console.log("I like ".concat(pizzas[i], " pizza."));
+}
+console.log('---');
+// Additional statement outside the loop
+console.log('I really love very type of pizza!');
+// example 35
+var animals = ['dog', 'cat', 'rabbit'];
+for (var i = 0; i < animals.length; i++) {
+    var animalName = animals[i];
+    console.log("The animal is ".concat(animalName));
+}
+console.log('Any of these animals would make a great pet!');
+// example 36
+function make_shirt(size, message) {
+    console.log("Size: ".concat(size));
+    console.log("Message: ".concat(message));
+}
+// Calling the function
+make_shirt('L', 'Hello, World!');
+function makeShirt(size, message) {
+    if (size === void 0) { size = 'large'; }
+    if (message === void 0) { message = 'I love TypeScript'; }
+    console.log("Creating a ".concat(size, " shirt with the message: ").concat(message));
+}
+makeShirt();
+makeShirt('medium');
+makeShirt('small', 'Hello, world!');
+// example 38
+function describeCity(city, country) {
+    if (country === void 0) { country = 'Unknown Country'; }
+    console.log("".concat(city, " is in ").concat(country, "."));
+}
+describeCity('Karachi', 'Pakistan');
+describeCity('London', 'United Kingdom');
+describeCity('New York');
+// example 39
+function city_country(city, country) {
+    return "".concat(city, ", ").concat(country);
+}
+console.log(city_country('Lahore', 'Pakistan')); // Output: Lahore, Pakistan
+console.log(city_country('London', 'United Kingdom')); // Output: London, United Kingdom
+console.log(city_country('Sydney', 'Australia'));
+// example 40
+function makeAlbum(artist, title, tracks) {
+    var album = {
+        artist: artist,
+        title: title,
+        tracks: tracks,
+    };
+    if (tracks) {
+        album.tracks = tracks;
+    }
+    return album;
+}
+var album1 = makeAlbum('Artist A', 'Album X');
+console.log(album1);
+var album2 = makeAlbum('Artist B', 'Album Y', 12); // includes the number of tracks
+console.log(album2);
+var album3 = makeAlbum('Artist C', 'Album Z');
+album3.tracks = 8;
+console.log(album3);
+// example 41
+function show_magicians(magicians) {
+    magicians.forEach(function (magician) {
+        console.log(magician);
+    });
+}
+var magicianNames = [
+    'Harry Houdini',
+    'David Copperfield',
+    'Penn Jillette',
+    'Teller',
+];
+show_magicians(magicianNames);
+var magicians = [
+    { name: 'Harry Houdini' },
+    { name: 'David Blaine' },
+    { name: 'Penn Jillette' },
+];
+function makeGreat(magicians) {
+    for (var _i = 0, magicians_1 = magicians; _i < magicians_1.length; _i++) {
+        var magician = magicians_1[_i];
+        magician.name = "the Great ".concat(magician.name);
+    }
+}
+function showMagicians(magicians) {
+    for (var _i = 0, magicians_2 = magicians; _i < magicians_2.length; _i++) {
+        var magician = magicians_2[_i];
+        console.log(magician.name);
+    }
+}
+makeGreat(magicians);
+showMagicians(magicians);
+// example 43
+function makeGreats(magicians) {
+    return magicians.map(function (name) { return "Great ".concat(name); });
+}
+function showMagicians1(magicians) {
+    console.log(magicians.join(', '));
+}
+var originalMagicians = [
+    'Harry Houdini',
+    'David Copperfield',
+    'Penn Jillette',
+];
+var newMagicians = makeGreats(originalMagicians);
+showMagicians1(newMagicians);
+// example 44
+function orderSandwich() {
+    var items = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        items[_i] = arguments[_i];
+    }
+    console.log('Order Summary:');
+    console.log("- Sandwich with ".concat(items.join(', ')));
+}
+orderSandwich('Ham', 'Cheese');
+orderSandwich('Turkey', 'Lettuce', 'Tomato');
+orderSandwich('Chicken', 'Bacon', 'Avocado', 'Mayonnaise');
+// example 45
+function createCar(manufacturer, model) {
+    var options = [];
+    for (var _i = 2; _i < arguments.length; _i++) {
+        options[_i - 2] = arguments[_i];
+    }
+    var car = { manufacturer: manufacturer, model: model };
+    options.forEach(function (_a) {
+        var key = _a[0], value = _a[1];
+        car[key] = value;
+    });
+    return car;
+}
+var myCar = createCar('Tesla', 'Model S', ['color', 'red'], ['optionalFeature', 'autopilot']);
+console.log(myCar);
--- index.ts
+++ index.ts
@@ -303,3 +303,459 @@ console.log('List of Famous Landmarks:');
 for (let i = 0; i < landmarking.length; i++) {
 	console.log(landmarking[i]);
 }
+
+// exercise 23
+
+let car: string = 'subaru';
+
+console.log("Is car == 'subaru'? I predict true.");
+console.log(car == 'subaru');
+
+console.log("Is car == 'honda'? I predict false.");
+console.log(car == 'honda');
+
+console.log("Is car != 'toyota'? I predict true.");
+console.log(car != 'toyota');
+
+console.log("Is car != 'subaru'? I predict false.");
+console.log(car != 'subaru');
+
+let age: number = 18;
+
+console.log('Is age >= 18? I predict true.');
+console.log(age >= 18);
+
+console.log('Is age < 21? I predict true.');
+console.log(age < 21);
+
+let name1 = 'Alice';
+
+console.log(`Does name start with letter 'A'? I predict True.`);
+console.log(name1 == 'Alice'); // true
+
+let name2 = 'Bob';
+
+console.log(`Does name end with letter 'a'? I predict False.`);
+console.log(name2 == 'Alice'); // false
+
+// exercise 24
+// Tests for equality and inequality with strings:
+const string1: string = 'Hello';
+const string2: string = 'World';
+
+console.log(string1 === string2);
+console.log(string1 !== string2);
+
+// Tests using the lower case function:
+
+const uppercaseString = 'HELLO';
+const lowercaseString = 'hello';
+
+console.log(uppercaseString.toLowerCase() === lowercaseString);
+console.log(lowercaseString.toLowerCase() !== uppercaseString);
+
+// Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to:
+
+const num1: number = 5;
+const num2: number = 10;
+
+console.log(num1 === num2);
+
+console.log(num1 !== num2);
+
+console.log(num1 > num2);
+
+console.log(num1 < num2);
+
+console.log(num1 >= 5 && num2 <= 10);
+
+// Tests using "and" and "or" operators:
+
+const x = true;
+const y = false;
+
+console.log(x && y);
+
+console.log(x || y);
+
+// Test whether an item is in an array:
+
+const arrays = [1, 2, 3, 4, 5];
+const item1 = 3;
+const item2 = 6;
+
+let isItemInArray1 = false;
+for (let i = 0; i < arrays.length; i++) {
+	if (arrays[i] === item1) {
+		isItemInArray1 = true;
+		break;
+	}
+}
+
+console.log(isItemInArray1); // true
+
+// example 25
+
+let alien_color: string = 'green';
+
+// Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+if (alien_color === 'green') {
+	console.log('The player just earned 5 points.');
+}
+
+// Version that passes the if test
+alien_color = 'green'; // Change alien's color to green
+
+if (alien_color === 'green') {
+	console.log('The player just earned 15 points.'); // Output: The player just earned 5 points.
+}
+
+// Version that fails the if test
+alien_color = 'red'; // Change alien's color to red
+
+if (alien_color === 'green') {
+	console.log('The player just earned 10 points.'); // No output as this version fails the condition
+}
+
+// example 26
+
+const alien_colors: string = 'red';
+
+if (alien_colors === 'green') {
+	console.log(
+		'Congratulations! You just earned 5 points for shooting the alien.'
+	);
+} else {
+	console.log(
+		'Congratulations! You just earned 10 points for shooting the alien.'
+	);
+}
+
+// example 27
+
+let alien_color1: string = 'yellow';
+
+if (alien_color1 === 'green') {
+	console.log('Congratulations! You just earned 5 points.');
+} else if (alien_color1 === 'yellow') {
+	console.log('Congratulations! You just earned 10 points.');
+} else if (alien_color1 === 'red') {
+	console.log('Congratulations! You just earned 15 points.');
+}
+
+// example 28
+
+const ages: number = 25;
+
+if (ages < 2) {
+	console.log('The person is a baby.');
+} else if (ages >= 2 && ages < 4) {
+	console.log('The person is a toddler.');
+} else if (ages >= 4 && ages < 13) {
+	console.log('The person is a kid.');
+} else if (ages >= 13 && ages < 20) {
+	console.log('The person is a teenager.');
+} else if (ages >= 20 && ages < 65) {
+	console.log('The person is an adult.');
+} else {
+	console.log('The person is an elder.');
+}
+
+// example 29
+
+const favoriteFruits: string[] = ['banana', 'apple', 'orange'];
+
+if (favoriteFruits[0] == 'banana') {
+	console.log('You really like bananas!');
+}
+
+if (favoriteFruits[1] == 'apple') {
+	console.log('You really like apples!');
+}
+
+if (favoriteFruits[2] == 'orange') {
+	console.log('You really like oranges!');
+}
+
+if (favoriteFruits[3] == 'strawberry') {
+	console.log('You really like strawberries!');
+} else {
+	console.log('Strawberries are not one of your favorite fruits.');
+}
+
+// example 30
+
+const usernames: string[] = ['admin', 'user1', 'user2', 'user3', 'user4'];
+
+for (let i = 0; i < usernames.length; i++) {
+	const username = usernames[i];
+
+	if (username === 'admin') {
+		console.log('Hello admin, would you like to see a status report?');
+	} else {
+		console.log(`Hello ${username}, thank you for logging in again.`);
+	}
+}
+
+// example 31
+
+const users: string[] = [];
+
+if (users.length === 0) {
+	console.log('No Users: We need to find some users!');
+} else {
+	for (let i = 0; i < users.length; i++) {
+		const username = users[i];
+		console.log(username);
+	}
+}
+
+// example 32
+
+const current_users: string[] = ['John', 'Sarah', 'Mike', 'Emma', 'Alex'];
+const new_users: string[] = ['David', 'John', 'Emily', 'SARAH'];
+
+for (let i = 0; i < new_users.length; i++) {
+	const usernameExists = current_users.some(
+		(user) => user.toLowerCase() === new_users[i].toLowerCase()
+	);
+
+	if (usernameExists) {
+		console.log(
+			`Username '${new_users[i]}' is not available. Please enter a new username.`
+		);
+	} else {
+		console.log(`Username '${new_users[i]}' is available.`);
+	}
+}
+
+// example 33
+
+const ordinalNumbers = [
+	'1st',
+	'2nd',
+	'3rd',
+	'4th',
+	'5th',
+	'6th',
+	'7th',
+	'8th',
+	'9th',
+];
+
+for (let i = 0; i < ordinalNumbers.length; i++) {
+	let result: string;
+
+	if (i === 0) {
+		result = `${ordinalNumbers[i]}`;
+	} else if (i === 1) {
+		result = `${ordinalNumbers[i]}`;
+	} else if (i === 2) {
+		result = `${ordinalNumbers[i]}`;
+	} else {
+		result = `${ordinalNumbers[i]}th`;
+	}
+
+	console.log(result);
+}
+
+// example 34
+
+const pizzas: string[] = ['Pepperoni', 'Margherita', 'Hawaiian'];
+
+// Loop to print the name of each pizza
+for (let i = 0; i < pizzas.length; i++) {
+	console.log(pizzas[i]);
+}
+
+console.log('---');
+
+// Loop to print a sentence about each pizza
+for (let i = 0; i < pizzas.length; i++) {
+	console.log(`I like ${pizzas[i]} pizza.`);
+}
+
+console.log('---');
+
+// Additional statement outside the loop
+console.log('I really love very type of pizza!');
+
+// example 35
+
+const animals: string[] = ['dog', 'cat', 'rabbit'];
+
+for (let i = 0; i < animals.length; i++) {
+	const animalName = animals[i];
+
+	console.log(`The animal is ${animalName}`);
+}
+
+console.log('Any of these animals would make a great pet!');
+
+// example 36
+
+function make_shirt(size: string, message: string): void {
+	console.log(`Size: ${size}`);
+	console.log(`Message: ${message}`);
+}
+
+// Calling the function
+make_shirt('L', 'Hello, World!');
+
+// example 37
+
+type ShirtSize = 'small' | 'medium' | 'large';
+
+function makeShirt(
+	size: ShirtSize = 'large',
+	message: string = 'I love TypeScript'
+): void {
+	console.log(`Creating a ${size} shirt with the message: ${message}`);
+}
+
+makeShirt();
+makeShirt('medium');
+makeShirt('small', 'Hello, world!');
+
+// example 38
+
+function describeCity(city: string, country: string = 'Unknown Country') {
+	console.log(`${city} is in ${country}.`);
+}
+
+describeCity('Karachi', 'Pakistan');
+describeCity('London', 'United Kingdom');
+describeCity('New York');
+
+// example 39
+
+function city_country(city: string, country: string) {
+	return `${city}, ${country}`;
+}
+
+console.log(city_country('Lahore', 'Pakistan')); // Output: Lahore, Pakistan
+console.log(city_country('London', 'United Kingdom')); // Output: London, United Kingdom
+console.log(city_country('Sydney', 'Australia'));
+
+// example 40
+
+function makeAlbum(artist: string, title: string, tracks?: number) {
+	const album = {
+		artist,
+		title,
+		tracks,
+	};
+
+	if (tracks) {
+		album.tracks = tracks;
+	}
+
+	return album;
+}
+
+const album1 = makeAlbum('Artist A', 'Album X');
+console.log(album1);
+
+const album2 = makeAlbum('Artist B', 'Album Y', 12); // includes the number of tracks
+console.log(album2);
+
+const album3 = makeAlbum('Artist C', 'Album Z');
+album3.tracks = 8;
+console.log(album3);
+
+// example 41
+
+function show_magicians(magicians: string[]): void {
+	magicians.forEach((magician) => {
+		console.log(magician);
+	});
+}
+
+const magicianNames: string[] = [
+	'Harry Houdini',
+	'David Copperfield',
+	'Penn Jillette',
+	'Teller',
+];
+
+show_magicians(magicianNames);
+
+// example 42
+
+interface Magician {
+	name: string;
+}
+
+const magicians: Magician[] = [
+	{ name: 'Harry Houdini' },
+	{ name: 'David Blaine' },
+	{ name: 'Penn Jillette' },
+];
+
+function makeGreat(magicians: Magician[]): void {
+	for (let magician of magicians) {
+		magician.name = `the Great ${magician.name}`;
+	}
+}
+
+function showMagicians(magicians: Magician[]): void {
+	for (let magician of magicians) {
+		console.log(magician.name);
+	}
+}
+
+makeGreat(magicians);
+showMagicians(magicians);
+
+// example 43
+function makeGreats(magicians: string[]) {
+	return magicians.map((name) => `Great ${name}`);
+}
+
+function showMagicians1(magicians: string[]): void {
+	console.log(magicians.join(', '));
+}
+
+const originalMagicians = [
+	'Harry Houdini',
+	'David Copperfield',
+	'Penn Jillette',
+];
+const newMagicians = makeGreats(originalMagicians);
+showMagicians1(newMagicians);
+
+// example 44
+
+function orderSandwich(...items: string[]) {
+	console.log('Order Summary:');
+	console.log(`- Sandwich with ${items.join(', ')}`);
+}
+
+orderSandwich('Ham', 'Cheese');
+orderSandwich('Turkey', 'Lettuce', 'Tomato');
+orderSandwich('Chicken', 'Bacon', 'Avocado', 'Mayonnaise');
+
+// example 45
+
+function createCar(
+	manufacturer: string,
+	model: string,
+	...options: Array<[string, any]>
+) {
+	const car = { manufacturer, model };
+
+	options.forEach(([key, value]) => {
+		car[key] = value;
+	});
+
+	return car;
+}
+
+const myCar = createCar(
+	'Tesla',
+	'Model S',
+	['color', 'red'],
+	['optionalFeature', 'autopilot']
+);
+
+console.log(myCar);
